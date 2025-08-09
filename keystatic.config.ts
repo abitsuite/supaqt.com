92a47f5c32088924890476caa6ab842e09847220
Shomari <nyusternie@sdot.io>
2025-08-09 10:30:14 -0400

    Update branding.

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,19 +1,19 @@
-import { articlesKs, authorsKs, categoriesKs } from "@/lib/keystatic";
-import { config } from "@keystatic/core";
+import { articlesKs, authorsKs, categoriesKs } from '@/lib/keystatic'
+import { config } from '@keystatic/core'
 
 export default config({
-  storage: {
-    kind: "local",
-  },
-  ui: {
-    brand: {
-      name: "Astro News",
+    storage: {
+        kind: 'local',
     },
-    navigation: ["---", "articles", "---", "authors", "categories"],
-  },
-  collections: {
-    articles: articlesKs,
-    authors: authorsKs,
-    categories: categoriesKs,
-  },
-});
+    ui: {
+        brand: {
+            name: 'SupaQt Alpha',
+        },
+        navigation: ['---', 'articles', '---', 'authors', 'categories'],
+    },
+    collections: {
+        articles: articlesKs,
+        authors: authorsKs,
+        categories: categoriesKs,
+    },
+})
