0262579795ae279bdeb3bebdb29fc8c2a567075a
Shomari <nyusternie@sdot.io>
2025-08-09 13:42:04 -0400

    Change to Spotlight.

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -1,7 +1,7 @@
 import type { Link } from '../types'
 
 export const SITE = {
-    title: 'SupaQt Alpha • Builders Showcase',
+    title: 'SupaQt Alpha • Builders Spotlight',
     description: 'Showcase for SupaQt Community of Builders',
     author: 'aBitSuite',
     url: 'https://supaqt.com',
